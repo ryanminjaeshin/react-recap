c502d85332c606bb964cd2b87c60195b71415f7f
Ryan Shin <ryanminjaeshin@gmail.com>
2023-06-11 23:38:04 +0900

    add navbar

--- src/App.js
+++ src/App.js
@@ -1,18 +1,14 @@
 import './App.css';
+import Navbar from './Navbar';
 
 function App() {
   const title = 'Welcome to the new blog';
-  const likes = 50;
-  const person = {name:'yoshi', age:50};
-  const link = "http://google.com";
 
   return (
     <div className="App">
+      <Navbar />
       <div className="content">
         <h1>{title}</h1>
-        <p>Liked {likes} times</p>
-        <p>{person.name}</p>
-        <a href={link}>Google</a>
       </div>
     </div>
   );
