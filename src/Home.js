a0d250fc9e951c0fdb0800e56f72714c36d14603
Ryan Shin <ryanminjaeshin@gmail.com>
2023-06-11 23:49:21 +0900

    add annoymous func

--- src/Home.js
+++ src/Home.js
@@ -4,10 +4,15 @@ const Home = () => {
         console.log('hello world');
     }
 
+    const handleClickAgain = name => {
+        console.log('hello ' + name);
+    }
+
     return (  
         <div className="home">
             <h2>HomePage</h2>
             <button onClick={handleClick}>Click Me</button>
+            <button onClick={() => handleClickAgain('Ryan')}>Click ME AGAIN</button>
         </div>
     );
 }
