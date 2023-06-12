c1157d67d485a23a1fead23245bd2241c12e5c48
Ryan Shin <ryanminjaeshin@gmail.com>
2023-06-13 01:07:55 +0900

    import useFetch

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,7 +1,9 @@
 import { useParams } from "react-router-dom/cjs/react-router-dom.min";
+import useFetch from './useFectch';
 
 const BlogDetails = () => {
     const { id } = useParams();
+    const { data, error, isPending } = useFetch('http://localhosts:8000/blogs/');
     return (
         <div className="blog-details">
             <h2>Blog Details</h2>
