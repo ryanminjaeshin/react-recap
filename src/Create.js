7810419391618356ecca6ea070265fd31f8b4426
Ryan Shin <ryanminjaeshin@gmail.com>
2023-06-13 01:17:43 +0900

    add title/body label/input

--- src/Create.js
+++ src/Create.js
@@ -2,6 +2,22 @@ const Create = () => {
     return ( 
         <div className="create">
             <h2>Add a New Blog</h2>
+            <form>
+                <label>Blog title:</label>
+                <input 
+                    type="text"
+                    required
+                />
+                <label>Blog body:</label>
+                <textarea>
+                    required
+                </textarea>
+                <label>Blog author:</label>
+                <select name="" id="">
+                    <option value="mario">mario</option>
+                    <option value="yoshi">yoshi</option>
+                </select>
+            </form>
         </div>
      );
 }
